dc11cd574e67e38a4257e78c103cafa70afb7d22
KIRIN_TING <kanchitpol29092003@gmail.com>
2026-02-05 17:07:41 +0700

    ddddd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://www.comdept.cmru.ac.th/65143329/intern-api/'
+    apiUrl: 'https://www.comdept.cmru.ac.th/65143329/intern-api'
 };
