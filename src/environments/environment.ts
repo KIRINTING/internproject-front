9de0f906b3b4269bbcc4af7c77586442831f1efd
KIRIN_TING <kanchitpol29092003@gmail.com>
2026-02-07 00:19:24 +0700

    dwadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://apimyproject-production.up.railway.app'
+    apiUrl: 'https://apimyproject-production.up.railway.app/api'
 };
